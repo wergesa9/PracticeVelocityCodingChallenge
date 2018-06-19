d727693c20b5301357b74a47f8058343ec4e26de
Alex Werges <wergesa9@gmail.com>
2018-06-19 14:28:19 -0500

    First commit

---
+++ CodeChallenge.Helper/MyXmlReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data;
+
+namespace CodeChallenge.Helper
+{
+    public class MyXmlReader
+    {
+        public List<string> GetButtonsFromXml()
+        {
+            string buttonXml = HttpContext.Current.Server.MapPath("~/App_Data/buttons.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(buttonXml);
+            List<string> buttons = new List<string>();
+            buttons = (from rows in ds.Tables[0].AsEnumerable()
+                       select rows[0].ToString()).ToList();
+            return buttons;
+        }
+    }
+}
---
+++ CodeChallenge/Controllers/DefaultController.cs
@@ -0,0 +1,44 @@
+﻿using CodeChallenge.Helper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeChallenge.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            //return View();
+            return View("Index", GetButtonsFromXml());
+        }
+
+        /// <summary>
+        /// Gathers the names of buttons to include in the program from the buttons.xml file. 
+        /// Add the counting logic with razor syntax to _CountView.cshtml for the different buttons.
+        /// </summary>
+        /// <returns>List of button names from xml file.</returns>
+        private List<string> GetButtonsFromXml()
+        {
+            MyXmlReader myXmlReader = new MyXmlReader();
+            List<string> buttons = myXmlReader.GetButtonsFromXml();
+            return buttons;
+        }
+
+        /// <summary>
+        /// Returns the CountView partial view, with the given evt.
+        /// </summary>
+        /// <param name="evt">the name of an existing event</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("GetCountView")]
+        public ActionResult GetCountView(string evt)
+        {
+            return PartialView("_CountView", evt);
+        }
+    }
+}
